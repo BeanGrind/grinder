ebf00bb3f7c070264d70ce282db19f0af651d505
matt62king <matt62king@gmail.com>
2020-05-23 00:22:20 -0500

    pattern validation

---
+++ src/main/java/grinder/util/Strings.java
@@ -0,0 +1,8 @@
+package grinder.util;
+
+public class Strings {
+
+    public static boolean isNullOrEmpty(String string) {
+        return string == null || "".equals(string);
+    }
+}
--- src/test/java/ops/TestBean.java
+++ src/test/java/ops/TestBean.java
@@ -15,6 +15,9 @@ public class TestBean implements Mergeable, Copyable, Mappable, Validatable {
     private String fieldOne;
     private String fieldTwo;
 
+    @Validate(pattern = "[0-9]{2}-[0-9]{3}")
+    private String patternField;
+
     @Copy.Ignore
     @Merge.Ignore
     @Mapping.Ignore
@@ -37,6 +40,14 @@ public class TestBean implements Mergeable, Copyable, Mappable, Validatable {
         this.fieldTwo = fieldTwo;
     }
 
+    public String getPatternField() {
+        return patternField;
+    }
+
+    public void setPatternField(String patternField) {
+        this.patternField = patternField;
+    }
+
     public String getIgnoredField() {
         return ignoredField;
     }
@@ -49,14 +60,15 @@ public class TestBean implements Mergeable, Copyable, Mappable, Validatable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        TestBean mergeBean = (TestBean) o;
-        return Objects.equals(fieldOne, mergeBean.fieldOne) &&
-                Objects.equals(fieldTwo, mergeBean.fieldTwo) &&
-                Objects.equals(ignoredField, mergeBean.ignoredField);
+        TestBean testBean = (TestBean) o;
+        return Objects.equals(fieldOne, testBean.fieldOne) &&
+                Objects.equals(fieldTwo, testBean.fieldTwo) &&
+                Objects.equals(patternField, testBean.patternField) &&
+                Objects.equals(ignoredField, testBean.ignoredField);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(fieldOne, fieldTwo, ignoredField);
+        return Objects.hash(fieldOne, fieldTwo, patternField, ignoredField);
     }
 }
